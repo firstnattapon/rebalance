2a4dc6bbaac4c931aa1c07eb513f91780cc4ff7c
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-10-13 07:21:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ returns = risk_models.returns_from_prices(data_ , log_returns=True)
 
 returns["sum"] = returns.sum(axis=1)
 returns["cum"] = returns['sum'].cumsum(axis=0)
-plt.figure(figsize=(16,8))
+plt.figure(figsize=(16,12))
 plt.plot(returns.cum)
 st.pyplot()
 # returns.cum.plot();
